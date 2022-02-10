83bf36bde84868ba70b5e34b3f5cce26d9b1fb9e
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-10 09:38:31 -0800

    some changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const server = require("./api/server.js");
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
